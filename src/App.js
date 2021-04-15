d0f51b283ed966acd9e0ee9d186df0360ec8322f
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-04-15 15:02:20 +1000

    moved footer component into app.js to avoid too many components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import WeatherApp from "./WeatherApp";
-import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <WeatherApp />
-      <Footer />
+      <footer className="footer">
+        <p>
+          <a href="https://github.com/leberSmeaton/thunders-react" target="blank">
+            Open sourced on GitHub
+          </a>
+          {" "} by {" "}
+          <a href="https://www.linkedin.com/in/bella-leber-smeaton/" target="blank">
+            Bella Leber Smeaton
+          </a> | Hosted by <a
+            href="https://thirsty-bell-09c7e4.netlify.app" target="blank">
+            Netlify
+          </a> | <a
+            href="mailto:leber.smeaton@yahoo.com" target="blank">
+              Contact + Feedback 
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
