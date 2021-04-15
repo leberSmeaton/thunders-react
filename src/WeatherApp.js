6aaf63939c959c0d24f18b39d9b1195b85a5f9d0
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-04-15 15:10:07 +1000

    added weather css specifically for  weather app.

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,11 +1,11 @@
 import React from "react";
 import BeachImage from "./BeachImage.svg"; // relative path to image
 import AppContainer from "./AppContainer";
+import "./Weather.css";
 
 export default function WeatherApp() {
   return (
     <div className="WeatherApp">
-      {/*mainImage cropped in CSS*/}
       <div>
         <img src={BeachImage} className="mainImage" alt={"two people on beach"} />
       </div>
