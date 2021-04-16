90190c50947d3d20dadaca48cc027870bef14f46
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-04-16 16:59:23 +1000

    fixed city name bug

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,55 +9,58 @@ export default function WeatherInfo(props){
           <button className="GeoTagButton" type="submit">
           <span roll="img">📍</span>
           </button>
-          <strong>
-            {props.data.name}
-          </strong>{" "}
-          | <span><FormattedDate date={props.data.date} /> </span>
+          <span>
+            {props.data.city}
+          </span>
+          <span className="spaceDateTime">
+            |
+          </span>
+          <span>
+            <FormattedDate date={props.data.date} />
+          </span>
         </h4>
       </div>
         
       <div className="MainRow">
-        <div>
-          <div className="card">
-            <div className="card-body">
-              <h1 className="card-title">
-                <span>
-                  <img
-                    src="https://openweathermap.org/img/wn/01d@2x.png"
-                    alt="Clear"
-                    className="icon"
-                  />
-                </span>
-                <span>
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="units">
-                  <button className="active">
-                    °C
-                  </button>{" "}
-                  |
-                  <button>
-                    °F
-                  </button>
-                </span>
-              </h1>
+        <div className="card">
+          <div className="card-body">
+            <h1 className="card-title">
+              <span>
+                <img
+                  src="https://openweathermap.org/img/wn/01d@2x.png"
+                  alt="Clear"
+                  className="icon"
+                />
+              </span>
+              <span>
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="units">
+                <button className="active">
+                  °C
+                </button>{" "}
+                |
+                <button>
+                  °F
+                </button>
+              </span>
+            </h1>
 
-              <p>
-                Feels like <strong>{Math.round(props.data.feelsLike)}</strong>°
-                <br />
-                <br />
-                <strong>{props.data.tempMax}</strong>° Max |{" "}
-                <span>{props.data.tempMin}</span>
-                ° Overnight Min
-                <br />
-                <br />
-                <strong>{Math.round(props.data.windSpeed)}</strong>km/h Wind
-                <br />
-                <strong>{props.data.tempHumidity}</strong>% Humidity
-                <br />
-                <strong className="description">{props.data.description}</strong>
-              </p>
-            </div>
+            <p>
+              Feels like <strong>{Math.round(props.data.feelsLike)}</strong>°
+              <br />
+              <br />
+              <strong>{props.data.tempMax}</strong>° Max |{" "}
+              <span>{props.data.tempMin}</span>
+              ° Overnight Min
+              <br />
+              <br />
+              <strong>{Math.round(props.data.windSpeed)}</strong>km/h Wind
+              <br />
+              <strong>{props.data.tempHumidity}</strong>% Humidity
+              <br />
+              <strong className="description">{props.data.description}</strong>
+            </p>
           </div>
         </div>
       </div>
