e1468e4e8bad39004ae23dd876ead7b0b764e72f
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-04-18 18:02:11 +1000

    tested console.log in GeoLocation.js, but not defined

--- src/GeoLocation.js
+++ src/GeoLocation.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function GeoLocation(props){
+  // console.log(props.getCurrentPosition); undefined? How to connect?
   return (
     <button className="GeoLocation GeoTagButton" type="submit" onClick={props.getCurrentPosition}>
       <span roll="img">📍</span>
